df7e8e293440b5cb93cb976e1553c55367d5c1b7
puyangsky <puyangsky@pku.edu.cn>
2016-08-22 09:15:34 -0700

    Added policy parse module

--- etcdserver/api/v2http/auth.go
+++ etcdserver/api/v2http/auth.go
@@ -1,15 +1,19 @@
 package v2http
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
 // etdited by puyangsky
+
+// definition of our wrapped request
 type request struct {
 	URL     string   `json:"URL"`
 	Method  string   `json:"method"`
@@ -33,6 +37,7 @@ func authorize(r *http.Request) bool {
 	pJSON, err := json.Marshal(p)
 	checkErr(err)
 
+	// for logging use
 	filename := "/home/pyt/k8slog/header.log"
 	tm := time.Now().Format("2006-01-02 15:04:05")
 	content := fmt.Sprintf("[%s]\tsubject: %s, method: %s, url: %s, json: %s\n", tm, subject, method, url, pJSON)
@@ -42,9 +47,51 @@ func authorize(r *http.Request) bool {
 	checkErr(err)
 	f.Close()
 
-	return ok
+	return ok && coreAuthorize(p)
 }
 
-func (p *request) Auth() bool {
+func loadPolicy() []string {
+	filename := "/home/pyt/k8slog/policy.txt"
+	f, _ := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	defer f.Close()
+	buf := make([]byte, 1024)
+	// use byteBuffer to accelerate appending all strings instead of +=
+	var sb bytes.Buffer
+	for {
+		n, _ := f.Read(buf)
+		if 0 == n {
+			break
+		}
+		s := string(buf[:n])
+		sb.WriteString(s)
+	}
+	policy := sb.String()
+	var policies []string
+	policies = strings.Split(policy, "\n\n")
+	// for item := range policies {
+	// 	fmt.Println(item, ">>>>>>", policies[item])
+	// }
+	return policies
+}
+
+func coreAuthorize(r *request) bool {
+	policy := loadPolicy()
+	for i := range policy {
+		lines := strings.Split(policy, "\n")
+		if len(lines) < 1 {
+			continue
+		}
+		isAllowedSubject := false
+		for j := 1; j < len(lines); j++ {
+			// as defualt there is only one subject per request
+			if lines[j] == r.Subject[0] {
+				isAllowedSubject = true
+			}
+		}
+		items := strings.Split(lines[0], ", ")
+		if r.URL == items[0] && r.Method == items[1] && isAllowedSubject {
+			return true
+		}
+	}
 	return false
 }
